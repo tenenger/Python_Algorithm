1110fa1b36a3166ee7688e223921b84b43bbb103
tenenger <88726990+tenenger@users.noreply.github.com>
2021-12-21 17:04:01 +0900

    updata

--- 백준(파이썬)/a.py
+++ 백준(파이썬)/a.py
@@ -1,13 +1,10 @@
-n = int(input())
-result = []
+import math
+n = input()
+d = [0] * 10
 
-i = 2
-while n != 1:
-    
-    if n % i == 0:
-        result.append(str(i))
-        n = n / i
-        i == 2
-    else:
-        i += 1
-print("\n".join(result))
+for i in n:
+    if int(i) == 9:
+        i = 6
+    d[int(i)] += 1
+d[6] = math.ceil(d[6]/2)
+print(max(d))
